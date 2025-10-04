3b34bbb1f3898621595328a46cfce5fc184154a0
codingashishdev <atankmember@gmail.com>
2025-10-05 00:44:13 +0530

    fix: update database connection to use environment variables for configuration

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,11 +1,14 @@
 import { Pool } from "pg";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const pool = new Pool({
-  user: "postgres",
-  password: "***REMOVED***",
-  host: "localhost",
-  port: 5433,
-  database: "postgres",
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT || "5432"),
+  database: process.env.DB_NAME || "postgres",
 });
 
 export default pool;
